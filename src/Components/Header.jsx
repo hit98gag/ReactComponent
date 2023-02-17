f0fdc6375e95dc6f0e6fd9093e241bf7eca06e5e
Hitesh Tiwari <72449162+hit98gag@users.noreply.github.com>
2023-02-17 20:09:30 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+function Header(){
+    return(
+        <div className="headercontainer">
+            <h1>Hitesh Tiwari</h1>
+            <p> Full Stack Developer</p>
+            <div><a href="">Linkedin</a> <a href="">Github</a></div>
+            <hr />
+        </div>
+    )
+}
+export default Header;
---
+++ src/Components/Hobbies.jsx
@@ -0,0 +1,21 @@
+
+
+import './Hobbies.css'
+
+function Hobbies(){
+    return(
+        <div className="hobbies">
+            <h1>Hobbies</h1>
+            <ol>
+                <li>Football</li>
+                <li>Dancing</li>
+                <li>Reading Books</li>
+                <li>Learning New Technology</li>
+                
+            </ol>
+            <hr />
+        </div>
+    )
+ }
+
+ export default Hobbies
